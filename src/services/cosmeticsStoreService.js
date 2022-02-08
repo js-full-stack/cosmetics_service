ded3d4014bbeb5eb1b6cdbfbce06c03da2223101
Alexandr Tichenko <js.fullstack.development@gmail.com>
2022-02-08 21:06:24 +0200

    update data

--- src/services/cosmeticsStoreService.js
+++ src/services/cosmeticsStoreService.js
@@ -1,7 +1,6 @@
 const { Product } = require("../db/productsShema");
 
 const fetchProductsService = async () => {
-  //* находим посты пользователя поле userId которого === переданному
   const products = await Product.find({});
   return products;
 };
